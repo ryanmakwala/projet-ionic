91b63e4174b6d52c96335c416a31638720b9ed37
ryan makwala <ryan_makwala@hotmail.com>
2019-05-27 15:57:13 +0200

    second commit

--- src/app/prescriptions/prescriptions.page.ts
+++ src/app/prescriptions/prescriptions.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 
 @Component({
@@ -6,4 +8,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './prescriptions.page.html',
   styleUrls: ['./prescriptions.page.scss'],
 })
-export class PrescriptionsPage {}
+export class PrescriptionsPage {
+    public items : any = [];   
+    constructor(public navCtrl: NavController, public http: Http){}
+   ionViewWillEnter(){this.load();}
+   // Retrieve the JSON encoded data from the remote server   
+   // Using Angular's Http class and an Observable - then   
+   // assign this to the items array for rendering to the HTML template
+    
+   load(){
+            this.http.get('http://localhost/CoursPHP/retrieve-data.php')     
+             .map(res => res.json())     
+             .subscribe(data => {this.items = data;});   
+          }
+          
+          // Allow navigation to the Prescriptions page for creating a new ent   
+          addEntry()
+          {
+            this.navCtrl.push('Prescriptions');
+          }
+
+          // Allow navigation to the Prescriptions page for amending an existing   
+          // (We supply the actual record to be amended, as this method's parameters   
+          // to the Prescriptions page   
+          viewEntry(param)
+          {
+            this.navCtrl.push('Prescriptions', param);
+          }
+       }
